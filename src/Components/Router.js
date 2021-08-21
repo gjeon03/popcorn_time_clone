6e9f75347631c75eba5fad3da54e855105b2a364
JeonGyeongYeon <gjeon0321@gmail.com>
2021-08-22 06:05:14 +0900

    api & container & loader

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -5,19 +5,24 @@ import {
   Route,
   Switch,
 } from "react-router-dom";
-import Home from "Routes/Home";
-import TV from "Routes/TV";
-import Search from "Routes/Search";
+import Home from "Routes/Home/index";
+import TV from "Routes/TV/index";
+import Search from "Routes/Search/index";
 import Header from "Components/Header";
+import Detail from "Routes/Detail/index";
 
-export default () => (
+const RouterDefault = () => (
   <Router>
     <Header />
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={TV} />
       <Route path="/search" exact component={Search} />
+      <Route path="/movie/:id" component={Detail} />
+      <Route path="/show/:id" component={Detail} />
       <Redirect from="*" to="/" />
     </Switch>
   </Router>
 );
+
+export default RouterDefault;
